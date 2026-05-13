f56006d82631373fc1a10cfa1ced8b089e466cb4
Prem Soni <premsoni0469@gmail.com>
2026-05-13 20:14:33 +0530

    🤫

--- Escrow.sol
+++ Escrow.sol
@@ -3,14 +3,36 @@ pragma solidity ^0.8.0;
 
 contract Escrow {
 
-    address payable public seller;
+    address public buyer;
+    address public seller;
 
-    constructor(address payable _seller) payable {
+    bool public buyerApproved;
+    bool public sellerApproved;
+
+    constructor(address _seller) payable {
+        buyer = msg.sender;
         seller = _seller;
     }
 
-    // Send money to seller
-    function releasePayment() public {
-        seller.transfer(address(this).balance);
+    function approve() public {
+
+        if(msg.sender == buyer)
+            buyerApproved = true;
+
+        if(msg.sender == seller)
+            sellerApproved = true;
+
+        if(buyerApproved && sellerApproved) {
+            payable(seller).transfer(
+                address(this).balance
+            );
+        }
     }
+     // Send money to seller
+    function releasePayment() public {
+        payable(seller).transfer(
+                address(this).balance
+            );
+    }
+
 }
